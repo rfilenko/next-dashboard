ea8118541ac7d8458981ad498ea9cfdca9198dd0
Roman Filenko <roman.filenko@gmail.com>
2025-05-07 13:08:41 +0200

    init app setup

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,9 +24,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased flex flex-col min-h-screen items-center justify-center`}>
         {children}
       </body>
     </html>
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,15 @@
-import Image from "next/image";
+'use client'
 
-export default function Home() {
+import { Button } from "@/components/ui/button";
+
+const Home = () => {
   return (
-    <div></div>
+    <div className="max-w-4xl mx-auto py-10 flex flex-col items-center justify-center h-full">
+      <h1 className="text-4xl font-bold">Home page</h1>
+      <p className="text-lg">Welcome to the home page</p>
+      <Button onClick={() => { console.log('Button clicked') }}>Button</Button>
+    </div>
   );
 }
+
+export default Home
