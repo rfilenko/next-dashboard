95eb7541a571def8f7eecacee0b7587dedc712be
Roman Filenko <roman.filenko@gmail.com>
2025-05-12 15:08:35 +0200

    revert index route

---
+++ app/(admin)/page.tsx
@@ -0,0 +1,15 @@
+import { UserInfo } from "@/components/user/UserInfo";
+import { redirect } from "next/navigation";
+import getSession from "@/lib/getSession";
+
+const HomePage = async () => {
+    const session = await getSession()
+
+    return (
+        <div className="max-w-4xl mx-auto py-10 h-full">
+            {session?.user ? <UserInfo user={session.user} /> : redirect('/login')}
+        </div>
+    )
+}
+
+export default HomePage
